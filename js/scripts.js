afe04a6e93023957535635d935d2d8bc21d5ad0e
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-15 10:58:25 -0700

    add assign id prototype and add to addContact prototype
    co-authored-by Cody Fritz <clanalia55@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+// Business Logic for Address Book
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+// Business Logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -7,3 +24,4 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
